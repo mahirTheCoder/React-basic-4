a4c2538678338a295a3804d23a4f800a2ffdadd3
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-08-17 21:25:20 +0600

    hook add

--- src/assets/Pages/Home.jsx
+++ src/assets/Pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+
+  const [alu, setAlu] = useState ('this is me first peramitter')
+
+
+
+// console.log(alu)
+
+
+
+
+const handleButton = (data)=>{
+  console.log(data)
+}
+
   return (
     <>
-    
+    <h1>{alu}</h1>
+    <button onClick={handleButton('data transfar way')}>show me</button>
     </>
   )
 }
